7d8ca2d9d0415e41e03398cd74804de95863ddb1
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-07-12 14:46:59 +0300

    response

--- handler.py
+++ handler.py
@@ -1,17 +1,19 @@
 import json
 
-
 def lambda_handler(event, context):
     telegram_message_raw = event['body']
     telegram_message = json.loads(telegram_message_raw)
     print(telegram_message['message'])
+    print(telegram_message['message']['chat']['id'])
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(get_chat(telegram_message))
     }
 
 def get_chat(body):
+    message = body['message']
+    chat_id = message['chat']['id']
     return {
         'chat_id': chat_id,
-        'text': 'Okay'
+        'text': 'you said: ' + message['text']
     }
